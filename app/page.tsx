d5bcd441cabc3e13ea956c87967342924a40ccfb
Mike <spormuv@gmail.com>
2023-07-18 02:53:21 +0300

    feat: add contact section

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,19 @@
 import About from '@/components/About';
-import Main from '@/components/Main';
+import Contact from '@/components/Contact';
+import Home from '@/components/Home';
 import Navbar from '@/components/Navbar';
 import Projects from '@/components/Projects';
 import Skills from '@/components/Skills';
 
-export default function Home() {
+export default function Main() {
   return (
     <>
       <Navbar />
-      <Main />
+      <Home />
       <About />
       <Skills />
       <Projects />
+      <Contact />
     </>
   );
 }
---
+++ components/Contact.tsx
@@ -0,0 +1,143 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { AiOutlineMail } from 'react-icons/ai';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import { HiOutlineChevronDoubleUp } from 'react-icons/hi';
+
+const Contact = () => {
+  return (
+    <section id="contact" className="w-full lg:h-screen">
+      <div className="max-w-[1240px] m-auto px-2 py-16 w-full">
+        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
+          Contact
+        </p>
+        <h2 className="py-4">Get In Touch</h2>
+
+        <div className="grid lg:grid-cols-5 gap-8">
+          {/* Left (up) side */}
+          <div className="lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4">
+            <div className="lg:p-4 h-full ">
+              <div className="relative aspect-video">
+                <Image
+                  className="rounded-xl object-cover"
+                  src="/assets/contact.jpg"
+                  alt="contact image"
+                  fill
+                />
+              </div>
+
+              <div>
+                <h2 className="py-2">Mike</h2>
+                <p>Front-End Developer</p>
+                <p className="py-4">
+                  I am available for freelance or full-time positions. Contact
+                  me and let&apos;s talk.
+                </p>
+              </div>
+
+              <div>
+                <p className="uppercase pt-8">Connect With Me</p>
+                <div className="flex items-center justify-between py-4">
+                  <a
+                    href="https://www.linkedin.com"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                      <FaLinkedinIn />
+                    </div>
+                  </a>
+                  <a
+                    href="https://github.com/spormuv"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                      <FaGithub />
+                    </div>
+                  </a>
+
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                    <AiOutlineMail />
+                  </div>
+                  <Link href="/resume">
+                    <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                      <BsFillPersonLinesFill />
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Right (down) side */}
+          <div className="lg:col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4">
+            <div className="p-4">
+              {/* Form */}
+              <form action="" method="post" encType="multipart/form-data">
+                <div className="grid md:grid-cols-2 gap-4 w-full py-2">
+                  <div className="flex flex-col">
+                    <label className="uppercase text-sm py-2">Name</label>
+                    <input
+                      className="border-2 rounded-lg p-3 flex border-gray-300"
+                      type="text"
+                      name="name"
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <label className="uppercase text-sm py-2">
+                      Phone Number
+                    </label>
+                    <input
+                      className="border-2 rounded-lg p-3 flex border-gray-300"
+                      type="text"
+                      name="phone"
+                    />
+                  </div>
+                </div>
+
+                <div className="flex flex-col py-2">
+                  <label className="uppercase text-sm py-2">Email</label>
+                  <input
+                    className="border-2 rounded-lg p-3 flex border-gray-300"
+                    type="email"
+                    name="email"
+                  />
+                </div>
+                <div className="flex flex-col py-2">
+                  <label className="uppercase text-sm py-2">Subject</label>
+                  <input
+                    className="border-2 rounded-lg p-3 flex border-gray-300"
+                    type="text"
+                    name="subject"
+                  />
+                </div>
+                <div className="flex flex-col py-2">
+                  <label className="uppercase text-sm py-2">Message</label>
+                  <textarea
+                    className="w-full resize-none border-2 rounded-lg p-3 border-gray-300"
+                    name="message"
+                  />
+                </div>
+                <button className="w-full p-4 text-gray-100 mt-4">
+                  Send Message
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex justify-center py-12">
+          <Link href="#home">
+            <div className="rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
+              <HiOutlineChevronDoubleUp className="text-[#5651e5]" size={30} />
+            </div>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
