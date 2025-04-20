5dfefeee67e5233fee2348f8bd449643249a42e2
Mike <spormuv@gmail.com>
2025-04-20 12:11:39 +0300

    fix: conditionally rendering demo preview icon

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -14,14 +14,14 @@ const Projects = () => {
   };
 
   return (
-    <section id="projects" className="w-full lg:min-h-screen px-6 pt-28">
-      <div className="max-w-[1240px] mx-auto">
-        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
+    <section id='projects' className='w-full lg:min-h-screen px-6 pt-28'>
+      <div className='max-w-[1240px] mx-auto'>
+        <p className='text-xl tracking-widest uppercase text-[#5651e5]'>
           Projects
         </p>
         <motion.div
-          initial="hidden"
-          whileInView="visible"
+          initial='hidden'
+          whileInView='visible'
           viewport={{ once: true, amount: 0.5 }}
           transition={{ duration: 0.5 }}
           variants={{
@@ -29,39 +29,41 @@ const Projects = () => {
             visible: { opacity: 1, x: 0 },
           }}
         >
-          <h2 className="py-4">My Recent Projects</h2>
+          <h2 className='py-4'>My Recent Projects</h2>
         </motion.div>
 
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 my-8">
+        <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-4 my-8'>
           {projectsData?.slice(0, nextItems).map((project, ind) => (
             <div
               key={ind}
-              className="p-4 rounded-lg shadow-lg shadow-gray-400 cursor-pointer"
+              className='p-4 rounded-lg shadow-lg shadow-gray-400 cursor-pointer'
             >
-              <div className="w-full relative group aspect-video">
+              <div className='w-full relative group aspect-video'>
                 <Image
                   fill
                   src={project.background}
                   alt={project.title}
-                  className="object-cover rounded-lg"
+                  className='object-cover rounded-lg'
                 />
-                <div className="absolute w-full h-full bg-gradient-to-r from-[#5651e5] to-[#709dff] opacity-90 top-0 left-0 z-10 flex items-center justify-center gap-10 scale-0 group-hover:scale-100 transition-all duration-300 ease-out rounded-lg">
-                  <a href={project.code} target="_blank">
-                    <AiFillGithub className="text-4xl hover:scale-125 transition-all duration-300" />
+                <div className='absolute w-full h-full bg-gradient-to-r from-[#5651e5] to-[#709dff] opacity-90 top-0 left-0 z-10 flex items-center justify-center gap-10 scale-0 group-hover:scale-100 transition-all duration-300 ease-out rounded-lg'>
+                  <a href={project.code} target='_blank'>
+                    <AiFillGithub className='text-4xl hover:scale-125 transition-all duration-300' />
                   </a>
-                  <a href={project.demo} target="_blank">
-                    <AiFillEye className="text-4xl hover:scale-125 transition-all duration-300" />
-                  </a>
+                  {project?.demo && (
+                    <a href={project.demo} target='_blank'>
+                      <AiFillEye className='text-4xl hover:scale-125 transition-all duration-300' />
+                    </a>
+                  )}
                 </div>
               </div>
-              <div className="mt-4 text-center font-bold uppercase">
+              <div className='mt-4 text-center font-bold uppercase'>
                 {project.title}
               </div>
-              <div className="flex mt-4 flex-wrap gap-2">
+              <div className='flex mt-4 flex-wrap gap-2'>
                 {project.tools.map((tool, ind) => (
                   <span
                     key={ind}
-                    className="shadow-sm shadow-gray-300 rounded-full py-2 px-3 text-sm"
+                    className='shadow-sm shadow-gray-300 rounded-full py-2 px-3 text-sm'
                   >
                     {tool}
                   </span>
@@ -72,9 +74,9 @@ const Projects = () => {
         </div>
 
         {nextItems < projectsData.length && (
-          <div className="text-center">
+          <div className='text-center'>
             <button
-              className="p-4 text-gray-100 mt-4 hover:scale-105 transition"
+              className='p-4 text-gray-100 mt-4 hover:scale-105 transition'
               onClick={handleLoadMore}
             >
               Show More
